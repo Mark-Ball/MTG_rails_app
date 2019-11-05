a4cbaeff029be257e2ebf2df300a3f945b2a9ac3
Mark-Ball <markball111@gmail.com>
2019-11-06 05:49:22 +1100

    added route for info page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #route for home page
   get "/", to: "pages#home", as: "root"
+  get "/info", to: "pages#info", as: "info" 
   
   #routes for listings
   get "/listings", to: "listings#index", as: "listings"
